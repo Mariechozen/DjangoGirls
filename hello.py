af630d100f51331bae0729523e52e3a61de41ecb
maria <lillyjones736@yahoo.com>
2016-09-24 13:01:19 +0300

    my second commit

--- hello.py
+++ hello.py
@@ -1,27 +1,6 @@
-while True:
-    try:
-        count= raw_input("Please enter the first number:")
-        count= int(count)
-        break
-    except ValueError:
-        print("Not a valid integer! Please try again..... ")
 
-while True:
-    try:
-        counter= raw_input("Please enter the second number:")
-        counter= int(counter)
-        break
-    except ValueError:
-        print("Not a valid integer! Please try again..... ")
+print "Great, you successfully entered an integer!"
+print "Great, you successfully entered an integer!"
 
-for num in range(counter):
-
-    if num % 3 == 0 and num % 5 == 0:
-        print("Fizz Buzz")
-    elif num % 3 == 0:
-        print ("Fizz")
-    elif num % 5 == 0:
-        print ("Buzz")
-    else:
-        print (num)
+print "Great, you successfully entered an integer!"
 print "Great, you successfully entered an integer!"
